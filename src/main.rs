78c15e73ca217fad1811b55bd46bdb4d731c9dbf
jonas089 <jonas089@gmail.com>
2023-02-25 15:24:27 +0100

    notes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use casper::EntryPoints;
 
 fn main(){
     let example: EntryPoints = EntryPoints{name:"total_supply".to_string(), args:"some_args".to_string()};
+    // what it will look like
+
+    /*
+        let example: EntryPoint = EntryPoint(...);
+        example.into => macro impl that creates a new EntryPoint form the struct and returns it as a TokenStream
+        EntryPoints::add_entry_point(example.into())
+
+        note that EntryPoint class is occupied, so maybe choose something else or not import it from casper_types
+
+    */
     let res = example.my_trait_function();
     println!("Res: {}", res);
 }
