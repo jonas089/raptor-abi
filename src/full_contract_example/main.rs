08c752629c51025326640c5edf5760102c55c915
jonas089 <jonas089@gmail.com>
2023-02-26 19:27:52 +0100

    preparing project structure for test of complete contract

---
+++ src/full_contract_example/main.rs
@@ -0,0 +1,1 @@
+// A complete casper contract using EP2
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod example_contract;
-use example_contract::test_ep_integration_EP2;
+mod ep2_test;
+use ep2_test::test_ep_integration_EP2;
 fn main(){
     let res: u64 = test_ep_integration_EP2();
     match res {
