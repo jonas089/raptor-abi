577b1b9fd0253fb85947173ca35c0f318d213b5d
jonas089 <jonas089@gmail.com>
2023-03-09 00:20:08 +0100

    outline in lib.rs at root

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// Exportable functions
+
+/*
+
+
+Stage 1:
+Ultimately, the code_generator features should be imported in a native Casper project / contract crate.
+This file serves as an access point for the code_generator proc macro that depends on the helpers crate.
+
+Stage 2:
+Compile and create a new .contract file format that combines metadata and contract wasm.
+=> generate a .contract file instead of a output.json file.
+
+*/
