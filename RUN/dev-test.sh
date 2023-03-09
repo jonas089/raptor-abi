984d576ce75359d3e5090bfe95767af7ac7a23dc
jonas089 <jonas089@gmail.com>
2023-03-09 15:20:22 +0100

    rename example

--- RUN/dev-test.sh
+++ RUN/dev-test.sh
@@ -1,1 +1,1 @@
-cd ../dev-src && rm -rf target output.json && cargo build --release --target wasm32-unknown-unknown
+cd ../ink-style-example-contract && rm -rf target output.json && cargo build --release --target wasm32-unknown-unknown
