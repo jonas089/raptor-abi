50676d0b2075843ec86e48c42b65583fdd2fbc68
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-11-03 14:50:03 +0100

    contract

--- casper-contracts/ink-style-example-contract/src/main.rs
+++ casper-contracts/ink-style-example-contract/src/main.rs
@@ -1,8 +1,45 @@
 #![allow(dead_code)]
 #![allow(unreachable_patterns)]
 #![no_std]
-mod ink;
-use ink::ink_casper_contract;
-fn main(){
-    let _res:u64 = ink_casper_contract();
+#![no_main]
+
+extern crate alloc;
+use alloc::{vec, vec::Vec};
+
+use helpers::types::{CasperKey, CasperU64, CasperU256, CasperU512, CasperString};
+use code_generator::{InkCasperMacro};
+use casper_types::{
+    CLType, EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, Parameter
+};
+extern crate serde_json;
+
+pub fn init(){
+    #[derive(Default)]
+    #[derive(InkCasperMacro)]
+    struct NewEntryPointArgs1{
+        sender:CasperString,
+        recipient:CasperString,
+        amount:CasperU64,
+        id_first:CasperU256,
+        key:CasperKey
+    }
+
+    #[derive(Default)]
+    #[derive(InkCasperMacro)]
+    struct NewEntryPointArgs2{
+        sender: CasperString,
+        recipient: CasperString,
+        amount: CasperU64,
+        id_first: CasperU64,
+        key: CasperKey
+    }
+
+    let ep = NewEntryPointArgs1::default();
+    let ep2 = NewEntryPointArgs2::default();
+    let result = ep.get_params();
+    let _result2 = ep2.get_params();
+
+    let mut eps = EntryPoints::new();
+    let entry_point = EntryPoint::new("test", result.clone(), CLType::Unit, EntryPointAccess::Public, EntryPointType::Contract);
+    eps.add_entry_point(entry_point);
 }
